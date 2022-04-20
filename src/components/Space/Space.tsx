3b74742b1c771ccc009c63fa56278b5b275e026f
Andrés <usuario616@gmail.com>
2022-04-21 00:11:26 +0200

    feature:  space background

---
+++ src/components/Space/Space.tsx
@@ -0,0 +1,12 @@
+import './Space.css';
+
+const Space = () => {
+  return (
+    <div className="wrapper">
+      <div className="stars"></div>
+      <div className="twinkling"></div>
+      {/* <div className="clouds"></div> */}
+    </div>
+  );
+};
+export default Space;

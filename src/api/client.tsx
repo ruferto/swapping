7a55aa4856ad49bf4877b95c5926e5860a33318c
Andrés <usuario616@gmail.com>
2022-04-20 20:01:02 +0200

    feat: client for API

---
+++ src/api/client.tsx
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+const client = axios.create({ baseURL: process.env.REACT_APP_API_BASE_URL });
+
+client.interceptors.response.use(
+  (response) => response.data,
+  (error) => {
+    if (!error.response) {
+      return Promise.reject({ message: error.message });
+    }
+    return Promise.reject({
+      message: error.response.statusText,
+      ...error.response,
+      ...error.response.data,
+      ...error.status,
+    });
+  },
+);
+
+export default client;
